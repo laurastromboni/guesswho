fc432751af8f35aaca912040ef79204692cfe67a
Laura Stromboni <laura.stromboni@live.fr>
2018-10-30 15:22:30 +0100

    first version

---
+++ js/script.js
@@ -0,0 +1,375 @@
+var people = [
+  { name: 'abi',
+    img: 'Avatar_Abi.png',
+    genre: 'genre-female',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-middle',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-no' },
+  { name: 'adele',
+    img: 'Avatar_Adele.png',
+    genre: 'genre-female',
+    hairColor: 'hair-ginger',
+    haircut: 'haircut-long',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'amine',
+    img: 'Avatar_Amine.png',
+    genre: 'genre-male',
+    hairColor: 'hair-black',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-metis',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'antoine',
+    img: 'Avatar_Antoine.png',
+    genre: 'genre-male',
+    hairColor: 'hair-black',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-yes',
+    skin: 'skin-white',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'cecile',
+    img: 'Avatar_Cecile.png',
+    genre: 'genre-female',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'chloe',
+    img: 'Avatar_Chloe.png',
+    genre: 'genre-female',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-middle',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'christian',
+    img: 'Avatar_Christian.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-black',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'corrado',
+    img: 'Avatar_Corrado.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'fareaha',
+    img: 'Avatar_Fareaha.png',
+    genre: 'genre-female',
+    hairColor: 'hair-black',
+    haircut: 'haircut-middle',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-metis',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'filippo',
+    img: 'Avatar_Filippo.png',
+    genre: 'genre-male',
+    hairColor: 'hair-black',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-yes',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'geoffroy',
+    img: 'Avatar_Geoffroy.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-yes',
+    skin: 'skin-metis',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'harnit',
+    img: 'Avatar_Harnit.png',
+    genre: 'genre-female',
+    hairColor: 'hair-black',
+    haircut: 'haircut-long',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-metis',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-no' },
+  { name: 'heather',
+    img: 'Avatar_Heather.png',
+    genre: 'genre-female',
+    hairColor: 'hair-black',
+    haircut: 'haircut-long',
+    eyesColor: 'eyes-green',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'helen',
+    img: 'Avatar_Helen.png',
+    genre: 'genre-female',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-middle',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-black',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'jean ni',
+    img: 'Avatar_Jean-Ni.png',
+    genre: 'genre-male',
+    hairColor: 'hair-blond',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'laura',
+    img: 'Avatar_Laura.png',
+    genre: 'genre-female',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-long',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'marie',
+    img: 'Avatar_Marie.png',
+    genre: 'genre-female',
+    hairColor: 'hair-blond',
+    haircut: 'haircut-middle',
+    eyesColor: 'eyes-blue',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'mathis',
+    img: 'Avatar_Mathis.png',
+    genre: 'genre-male',
+    hairColor: 'hair-blond',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-green',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'mehdi',
+    img: 'Avatar_Mehdi.png',
+    genre: 'genre-male',
+    hairColor: 'hair-black',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-yes',
+    skin: 'skin-metis',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'niccolo',
+    img: 'Avatar_Niccolo.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'nicolas',
+    img: 'Avatar_Nicolas.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'nizar',
+    img: 'Avatar_Nizar.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-yes',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-no' },
+  { name: 'paul',
+    img: 'Avatar_Paul.png',
+    genre: 'genre-male',
+    hairColor: 'hair-black',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-blue',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' },
+  { name: 'priyanka',
+    img: 'Avatar_Priyanka.png',
+    genre: 'genre-female',
+    hairColor: 'hair-black',
+    haircut: 'haircut-middle',
+    eyesColor: 'eyes-brown',
+    facialHair: 'facialhair-no',
+    skin: 'skin-metis',
+    glasses: 'glasses-yes',
+    frenchNative: 'frenchnative-no' },
+  { name: 'regis',
+    img: 'Avatar_Regis.png',
+    genre: 'genre-male',
+    hairColor: 'hair-brown',
+    haircut: 'haircut-short',
+    eyesColor: 'eyes-blue',
+    facialHair: 'facialhair-no',
+    skin: 'skin-white',
+    glasses: 'glasses-no',
+    frenchNative: 'frenchnative-yes' }
+];
+
+$(document).ready(function(){
+  var guessWho = new GuessWho(people);
+
+  // use the shuffle function to shuffle randomnly the cards
+  guessWho.shuffleCards();
+
+  // show the popup welcome at the begining of the game, when we refresh the page
+  var startgame = $('.popup-welcome');
+  startgame.css({"display" : "block"});
+  $(".game").addClass("blocked");
+
+  // Array of every people
+  console.log(guessWho.people);
+  // create the HTML divs for each people
+  var html = '';
+  guessWho.people.forEach(function (people) {
+    html += '<div class="people ' + people.genre + ' ' + people.hairColor + ' '  + people.haircut + ' '  + people.eyesColor + ' '  + people.facialHair + ' '  + people.skin + ' '  + people.glasses + ' '  + people.frenchNative + ' " data-people-name="'+ people.name +'">';
+    html += '  <div class="front-people" style="background: url(img/'+ people.img + '); background-size: contain;background-position: center;background-repeat: no-repeat"></div>';
+    html += '  <div class="back-people"  style="background-color: #F8F5F5" name="'+ people.img +'"></div>';
+    html += '</div>';
+  });
+  // Add all the div's to the HTML
+  $('#avatars-board').html(html);
+
+  var guessWhoPeople = people[Math.floor(Math.random()*people.length)];
+  console.log(guessWhoPeople);
+
+  // main feature : hide the wrong people after clicking on caracteristics buttons
+  var buttons = $(".caracteristic");
+  var guessWho = new GuessWho(people);
+  buttons.click(function (e){
+    guessWho.updateChances()
+    console.log(e.currentTarget);
+    var firstClass = e.currentTarget.className.split(' ')[1];
+    var secondClass = e.currentTarget.className.split(' ')[2];
+    var people = $(".people");
+    people.each(function(index){
+      console.log(guessWhoPeople[firstClass])
+      if (guessWhoPeople[firstClass] === secondClass){
+        if (!$(this).hasClass(secondClass)){
+          $(this).css({"opacity" : "0"});
+          $(this).addClass("hidden-people");
+        } 
+      } else {
+        if ($(this).hasClass(secondClass)){
+          $(this).css({"opacity" : "0"});
+          $(this).addClass("hidden-people");
+        }
+      }
+    })
+    guessWho.isFinished()
+  });
+
+  // click directly on people to try to find the guessWhoPeople
+  var peoplebutton = $(".people");
+  peoplebutton.click(function (e){
+    guessWho.updateChances()
+    var pers = e.currentTarget;
+    var winner = $('.popup-winer');
+    if (pers.getAttribute("data-people-name") === guessWhoPeople.name){
+      winner.css({"display" : "block"});
+      $(".game").addClass("blocked");
+    } else {
+      $(pers).css({"opacity" : "0"});
+      $(pers).addClass("hidden-people");
+    }
+  })
+
+  // enter in the game when we click on the start button at the begining of the game
+  var startbtn = $(".btn-popup-welcome");
+  startbtn.click(function (){
+    var start = $('.popup-welcome');
+    start.css({"display" : "none"});
+    $(".game").removeClass("blocked");
+  })
+
+});
+
+// class GuesWho
+  class GuessWho {
+
+    constructor(people){
+    this.people = people;
+    this.remainingChances = 5;
+    }
+  
+   // shuffle randomnly the cards 
+   shuffleCards() {
+      var m = this.people.length, t, i;
+  
+      // While there remain elements to shuffle…
+      while (m) {
+  
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+  
+        // And swap it with the current element.
+        t = this.people[m];
+        this.people[m] = this.people[i];
+        this.people[i] = t;
+      }
+    }
+
+    updateChances() {
+      var looser = $('.popup-looser');
+      this.remainingChances --;
+      $("#remaining-chances").html(this.remainingChances);
+      if (this.remainingChances === 0){
+        looser.css({"display" : "block"});
+        $(".game").addClass("blocked");
+      }
+    }
+
+    isFinished(){
+      if ( $(".people").length - $(".hidden-people").length === 1){
+        var winner = $('.popup-winer');
+        winner.css({"display" : "block"});
+        $(".game").addClass("blocked");
+      } 
+    } 
+
+};
